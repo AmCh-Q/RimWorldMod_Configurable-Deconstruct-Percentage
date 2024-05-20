5d8419e0d5e5647251c625c1ff3459b423a26bec
AmCh-Q <AmCh-Q@users.noreply.github.com>
2024-05-20 16:01:23 -0400

    More Config Options 更多设置选项 Configurable return percentage when failing construction (Vanilla: 50%, mod default: 80%) Configurable return percentage when building is violently destroyed (Vanilla: 25%, mod default: 50%) Configurable minimum number of each item to return (mod default: 1 of each item) Simplified & Traditional Chinese support 建造失败时返还百分比可设置（原版：50%，模组默认：80%） 建筑摧毁时返还百分比可设置（原版：25%，模组默认：50%） 每种建筑材料至少返回X份（模组默认：X=1） 简体&繁体中文

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using RimWorld;
 using UnityEngine;
 using Verse;
 
@@ -6,59 +6,94 @@ namespace Configurable_Deconstruct_Percentage
 {
     public class ConfigurableDeconstructPercentage : Mod
     {
-        private readonly DeconstructSettings settings;
         public ConfigurableDeconstructPercentage(ModContentPack content)
             : base(content)
         {
-            settings = GetSettings<DeconstructSettings>();
+            GetSettings<DeconstructSettings>();
             LongEventHandler.QueueLongEvent(delegate
             {
-                settings.ModifyDeconstructionFraction();
-            }, "Config_Deconstruct_Percentage", false, null);
+                DeconstructSettings.UpdateDeconstructionFraction();
+#if !v1_0 && !v1_1
+                Patch.ApplyPatches();
+#endif
+            }, "ConfigurableDeconstructPercentage", false, null);
+        }
+        private static void AddNumSettings(Listing_Standard ls, ref int percent, string labelKey)
+        {
+            int num = percent;
+            string labelStr = ("DecPerc." + labelKey).Translate(num);
+            string tipStr = ("DecPerc." + labelKey + "_Tip").Translate(num);
+#if v1_0 || v1_1 || v1_2 || v1_3
+            ls.Label(labelStr, -1f, tipStr);
+            num = Mathf.RoundToInt(ls.Slider(num, 0f, 100f));
+#else
+            num = Mathf.RoundToInt(ls.SliderLabeled(labelStr, num, 0f, 100f, labelPct: 0.4f, tooltip: tipStr));
+#endif
+            string buffer = num.ToString();
+            ls.TextFieldNumeric(ref num, ref buffer, 0f, 100f);
+            percent = num;
         }
         public override void DoSettingsWindowContents(Rect inRect)
         {
             base.DoSettingsWindowContents(inRect);
             Listing_Standard ls = new Listing_Standard();
             ls.Begin(inRect);
-            int deconstructionPercent = settings.DeconstructionPercent;
-            ls.Label("DecPerc.Setting".Translate(deconstructionPercent), 
-                -1f, "DecPerc.RestartNReq".Translate());
-            deconstructionPercent = Mathf.RoundToInt(ls.Slider(deconstructionPercent, 0f, 100f));
-            string buffer = deconstructionPercent.ToString();
-            ls.TextFieldNumeric(ref deconstructionPercent, ref buffer, 0f, 100f);
-            settings.DeconstructionPercent = deconstructionPercent;
+            ls.Label("DecPerc.RestartNReq".Translate());
+            AddNumSettings(ls, ref DeconstructSettings.DeconstructPercent,
+                "DeconstructPercent");
+#if !v1_0 && !v1_1
+            AddNumSettings(ls, ref DeconstructSettings.FailReturnPercent,
+                "FailReturnPercent");
+            AddNumSettings(ls, ref DeconstructSettings.DestroyReturnPercent,
+                "DestroyReturnPercent");
+            AddNumSettings(ls, ref DeconstructSettings.MinimumReturn,
+                "MinimumReturn");
+#endif
             ls.End();
         }
         public override string SettingsCategory()
-        {
-            return "DecPerc.Name".Translate();
-        }
+            => "DecPerc.Name".Translate();
     }
 
-    public class DeconstructSettings : ModSettings
+    public partial class DeconstructSettings : ModSettings
     {
-        public int DeconstructionPercent = 100;
+        public static int DeconstructPercent = 100;
+#if !v1_0 && !v1_1
+        public static int FailReturnPercent = 100;
+        public static int DestroyReturnPercent = 100;
+        public static int MinimumReturn = 1;
+#endif
         public override void ExposeData()
         {
-            Scribe_Values.Look(ref DeconstructionPercent, "Deconstruct_Percent", 0);
             base.ExposeData();
-            ModifyDeconstructionFraction();
+            Scribe_Values.Look(ref DeconstructPercent, nameof(DeconstructPercent), 100);
+#if !v1_0 && !v1_1
+            Scribe_Values.Look(ref FailReturnPercent, nameof(FailReturnPercent), 80);
+            Scribe_Values.Look(ref DestroyReturnPercent, nameof(DestroyReturnPercent), 50);
+            Scribe_Values.Look(ref MinimumReturn, nameof(MinimumReturn), 1);
+#endif
+            UpdateDeconstructionFraction();
         }
-        internal void ModifyDeconstructionFraction()
+        internal static void UpdateDeconstructionFraction()
         {
 #if v1_0
             foreach (TerrainDef allDef in DefDatabase<TerrainDef>.AllDefs)
-                allDef.resourcesFractionWhenDeconstructed = DeconstructionPercent * 0.01f;
+                allDef.resourcesFractionWhenDeconstructed = DeconstructPercent * 0.01f;
             foreach (ThingDef allDef in DefDatabase<ThingDef>.AllDefs)
-                allDef.resourcesFractionWhenDeconstructed = DeconstructionPercent * 0.01f;
-#else
+                allDef.resourcesFractionWhenDeconstructed = DeconstructPercent * 0.01f;
+#elif v1_1
             foreach (BuildableDef allDef in DefDatabase<BuildableDef>.AllDefs)
-                allDef.resourcesFractionWhenDeconstructed = DeconstructionPercent * 0.01f;
+                allDef.resourcesFractionWhenDeconstructed = DeconstructPercent * 0.01f;
 #endif
-            Log.Message(
-                "[Configurable Deconstruct Percentage]: Deconstruction return percentage modified to " 
-                + DeconstructionPercent.ToString() + "%");
+            Log.Message(string.Concat(
+                "[Configurable Deconstruct Percentage]: ",
+                nameof(DeconstructPercent), " = " , DeconstructPercent.ToString(), "%, "
+#if !v1_0 && !v1_1
+                , nameof(FailReturnPercent), " = ", FailReturnPercent.ToString(), "%, ",
+                nameof(DestroyReturnPercent), " = ", DestroyReturnPercent.ToString(), "%, ",
+                nameof(MinimumReturn), " = ", MinimumReturn.ToString()
+#endif
+            ));
         }
     }
 }
